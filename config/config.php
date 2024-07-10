5f3140302dc6eb20fceda5935dfeb70b2c06a6c8
Robert Hartl <1510875+hartl@users.noreply.github.com>
2024-07-10 13:52:25 +0200

    Update config.php

--- config/config.php
+++ config/config.php
@@ -16,23 +16,26 @@ $GLOBALS['BE_FFL']['tag'] = 'TagField';
 /**
  * Front end modules
  */
-array_insert($GLOBALS['FE_MOD']['tags'], 1, array
-(
-	'tagcloud'            => 'ModuleTagCloud',
-  'tagcloudarticles'    => 'ModuleTagCloudArticles',
-	'taggedArticleList'   => 'ModuleTaggedArticleList',
-  'tagscope'            => 'ModuleTagScope',
-  'tagcontentlist'      => 'ModuleTagContentList',
-  'taglistbycategory'   => 'ModuleTagListByCategory',
-  'tagcloudcontent'     => 'ModuleTagCloudContent',
-  'tagcloudevents'      => 'ModuleTagCloudEvents',
-  'tagcloudmembers'     => 'ModuleTagCloudMembers',
-  'tagcloudnews'        => 'ModuleTagCloudNews'
-));
-array_insert($GLOBALS['FE_MOD']['miscellaneous'], 3, array
-(
-	'globalArticleList'    => 'ModuleGlobalArticlelist'
-));
+
+// Statt array_insert
+$fe_mod_tags = array(
+    'tagcloud'            => 'ModuleTagCloud',
+    'tagcloudarticles'    => 'ModuleTagCloudArticles',
+    'taggedArticleList'   => 'ModuleTaggedArticleList',
+    'tagscope'            => 'ModuleTagScope',
+    'tagcontentlist'      => 'ModuleTagContentList',
+    'taglistbycategory'   => 'ModuleTagListByCategory',
+    'tagcloudcontent'     => 'ModuleTagCloudContent',
+    'tagcloudevents'      => 'ModuleTagCloudEvents',
+    'tagcloudmembers'     => 'ModuleTagCloudMembers',
+    'tagcloudnews'        => 'ModuleTagCloudNews'
+);
+array_splice($GLOBALS['FE_MOD']['tags'], 1, 0, $fe_mod_tags);
+
+$misc_mods = array(
+    'globalArticleList'    => 'ModuleGlobalArticlelist'
+);
+array_splice($GLOBALS['FE_MOD']['miscellaneous'], 3, 0, $misc_mods);
 
 $GLOBALS['FE_MOD']['news']['newslist'] = 'ModuleNewsListTags';
 $GLOBALS['FE_MOD']['news']['newsarchive'] = 'ModuleNewsArchiveTags';
@@ -42,43 +45,34 @@ $GLOBALS['FE_MOD']['events']['eventlist'] = 'ModuleEventlistTags';
 $GLOBALS['FE_MOD']['events']['eventreader'] = 'ModuleEventReaderTags';
 $GLOBALS['FE_MOD']['faq']['faqlist'] = 'ModuleFaqListTags';
 
-if (array_key_exists('last_events', $GLOBALS['FE_MOD']['events']))
-{
-	// add support for last_events extension
-	$GLOBALS['FE_MOD']['events']['last_events'] = 'ModuleFaqListTagstsTags';
+if (array_key_exists('last_events', $GLOBALS['FE_MOD']['events'])) {
+    // add support for last_events extension
+    $GLOBALS['FE_MOD']['events']['last_events'] = 'ModuleFaqListTagstsTags';
 }
 
 /**
  * Content elements
-	*/
+ */
 $GLOBALS['TL_CTE']['texts']['headline'] = 'ContentHeadlineTags';
 $GLOBALS['TL_CTE']['media']['gallery'] = 'ContentGalleryTags';
 
-if (TL_MODE == 'BE')
-{
-	/**
-	 * CSS files
-	 */
-
-    if (isset($GLOBALS['TL_CSS']) && \is_array($GLOBALS['TL_CSS']))
-	{
-		array_insert($GLOBALS['TL_CSS'], 1, 'system/modules/tags/assets/tag.css');
-	}
-	else
-	{
-		$GLOBALS['TL_CSS'] = array('system/modules/tags/assets/tag.css');
-	}
-
-	/**
-	 * JavaScript files
-	 */
-    if (isset($GLOBALS['TL_JAVASCRIPT']) && \is_array($GLOBALS['TL_JAVASCRIPT']))
-    {
-        \array_insert($GLOBALS['TL_JAVASCRIPT'], 1, 'system/modules/tags/assets/tag.js');
+if (TL_MODE == 'BE') {
+    /**
+     * CSS files
+     */
+    if (isset($GLOBALS['TL_CSS']) && \is_array($GLOBALS['TL_CSS'])) {
+        array_splice($GLOBALS['TL_CSS'], 1, 0, ['system/modules/tags/assets/tag.css']);
+    } else {
+        $GLOBALS['TL_CSS'] = ['system/modules/tags/assets/tag.css'];
     }
-    else
-    {
-        $GLOBALS['TL_JAVASCRIPT'] = array('system/modules/tags/assets/tag.js');
+
+    /**
+     * JavaScript files
+     */
+    if (isset($GLOBALS['TL_JAVASCRIPT']) && \is_array($GLOBALS['TL_JAVASCRIPT'])) {
+        array_splice($GLOBALS['TL_JAVASCRIPT'], 1, 0, ['system/modules/tags/assets/tag.js']);
+    } else {
+        $GLOBALS['TL_JAVASCRIPT'] = ['system/modules/tags/assets/tag.js'];
     }
 }
 
@@ -107,29 +101,23 @@ $GLOBALS['tags_extension']['sourcetable'][] = 'fl_files';
 * Add 'tag' to the URL keywords to prevent problems with URL manipulating modules like folderurl
 */
 if (isset($GLOBALS['TL_CONFIG']['urlKeywords'])) {
-	$GLOBALS['TL_CONFIG']['urlKeywords'] .= (strlen(trim($GLOBALS['TL_CONFIG']['urlKeywords'])) ? ',' : '') . 'tag';
+    $GLOBALS['TL_CONFIG']['urlKeywords'] .= (strlen(trim($GLOBALS['TL_CONFIG']['urlKeywords'])) ? ',' : '') . 'tag';
 } else {
-	$GLOBALS['TL_CONFIG']['urlKeywords'] = 'tag';
+    $GLOBALS['TL_CONFIG']['urlKeywords'] = 'tag';
 }
 $GLOBALS['tags']['showInFeeds'] = true;
 
 $GLOBALS['TL_FFL']['tag'] = 'TagFieldMemberFrontend';
 
-
-
-if (is_array($GLOBALS['TL_CRON']['daily']))
-{
-	foreach ($GLOBALS['TL_CRON']['daily'] as $key => $arr)
-	{
-		if (is_array($arr) && strcmp($arr[0], 'Calendar') == 0 && strcmp($arr[1], 'generateFeeds') == 0)
-		{
-			// Fix calendar feed cron job
-			$GLOBALS['TL_CRON']['daily'][$key] = array('CalendarTags', 'generateFeeds');
-		}
-		if (is_array($arr) && strcmp($arr[0], 'News') == 0 && strcmp($arr[1], 'generateFeeds') == 0)
-		{
-			// Fix news feed cron job
-			$GLOBALS['TL_CRON']['daily'][$key] = array('NewsTags', 'generateFeeds');
-		}
-	}
+if (is_array($GLOBALS['TL_CRON']['daily'])) {
+    foreach ($GLOBALS['TL_CRON']['daily'] as $key => $arr) {
+        if (is_array($arr) && strcmp($arr[0], 'Calendar') == 0 && strcmp($arr[1], 'generateFeeds') == 0) {
+            // Fix calendar feed cron job
+            $GLOBALS['TL_CRON']['daily'][$key] = array('CalendarTags', 'generateFeeds');
+        }
+        if (is_array($arr) && strcmp($arr[0], 'News') == 0 && strcmp($arr[1], 'generateFeeds') == 0) {
+            // Fix news feed cron job
+            $GLOBALS['TL_CRON']['daily'][$key] = array('NewsTags', 'generateFeeds');
+        }
+    }
 }
